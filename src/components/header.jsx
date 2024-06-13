cbcab3b6884b79ac9382e8ed576667cb714c5fbc
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-06-13 21:32:03 +0530

    Header css updated with screen size response

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,22 +5,22 @@ function Header() {
     return (
         <div className="header">
             <img src="/images/logo.png" alt="Disha AI logo"/>
-            <div className="nav-links space-x-6">
+            <div className="nav-links">
                 <a href="#home">HOME</a>
                 <a href="#genesis">Genesis of DISHA AI</a>
                 <a href="#differentiator">DISHA AI Differentiator</a>
                 <a href="#reach">Reach Us</a>
             </div>
-            <div className="header-buttons space-x-5">
+            <button
+                className="SEP cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
+                SEP
+            </button>
+            <Link to="/login">
                 <button
-                    className="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
-                    SEP
-                </button>
-                <Link to="/login"><button
-                    className="cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
+                    className="LOGIN cursor-pointer transition-all bg-blue-500 text-white px-6 py-2 rounded-lg border-blue-600 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]">
                     LOGIN
-                </button></Link>
-            </div>
+                </button>
+            </Link>
         </div>
     );
 }
