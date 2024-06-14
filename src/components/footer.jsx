5860cc048c1591ab79d29761cf0255b52604b3b8
siddu015 <116783967+siddu015@users.noreply.github.com>
2024-06-14 17:56:19 +0530

    Footer added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,27 +1,85 @@
-export default function Footer()
-{
-    return(
-            <div className="flex w-screen bg-blue-200  p-4 justify-between">
-                <div className="grid gap-4">
-                    <h1 className="font-foot underline underline-offset-4 text-3xl text-center">CONTACT US</h1>
-                <span className="flex gap-2">
-                    <img src="/icons/mail.svg" className="bg-blue-900 rounded-full p-2 w-10 h-10"/><h1 className="font-sans text-xl font-bold">syam@dishaai.com</h1>
-                </span>
-                <span className="flex gap-2">
-                    <img src="/icons/phone.svg" className="bg-blue-900 rounded-full p-2 w-10 h-10"/><h1 className="font-sans text-xl font-bold">+917330751516</h1>
-                </span>
-                </div>
-                <div>
-                <h1 className="font-foot underline underline-offset-4 text-center text-3xl">SOCIAL MEDIA</h1>
-                <div className="flex">
-                <img src="/icons/facebook.svg" className="social hover:bg-[#5293ff]"/>
-                <img src="/icons/instagram.svg" className="social hover:bg-[#d300c5]"/>
-                <img src="/icons/x.svg" className="social"/>
-                <img src="/icons/linkedin.svg" className="social hover:bg-[#0a66c2]"/>
-                </div>
-                <div>
-                </div>
-                </div>
+import './footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer bg-gray-100 text-gray-800 p-8">
+            <div className="social-container">
+                <ul className="social flex mb-6 md:mb-0">
+                    <li className="social-item">
+                        <a className="social-link" href="/">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M9.19795 21.5H13.198V13.4901H16.8021L17.198 9.50977H13.198V7.5C13.198 6.94772 13.6457 6.5 14.198 6.5H17.198V2.5H14.198C11.4365 2.5 9.19795 4.73858 9.19795 7.5V9.50977H7.19795L6.80206 13.4901H9.19795V21.5Z"
+                                    fill="currentColor"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li className="social-item">
+                        <a className="social-link" href="/">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd"
+                                      d="M8 3C9.10457 3 10 3.89543 10 5V8H16C17.1046 8 18 8.89543 18 10C18 11.1046 17.1046 12 16 12H10V14C10 15.6569 11.3431 17 13 17H16C17.1046 17 18 17.8954 18 19C18 20.1046 17.1046 21 16 21H13C9.13401 21 6 17.866 6 14V5C6 3.89543 6.89543 3 8 3Z"
+                                      fill="currentColor"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li className="social-item">
+                        <a className="social-link" href="/">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M6 12C6 15.3137 8.68629 18 12 18C14.6124 18 16.8349 16.3304 17.6586 14H12V10H21.8047V14H21.8C20.8734 18.5645 16.8379 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C15.445 2 18.4831 3.742 20.2815 6.39318L17.0039 8.68815C15.9296 7.06812 14.0895 6 12 6C8.68629 6 6 8.68629 6 12Z"
+                                    fill="currentColor"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li className="social-item">
+                        <a className="social-link" href="/">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" clipRule="evenodd"
+                                      d="M12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7ZM9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12Z"
+                                      fill="currentColor"></path>
+                                <path
+                                    d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
+                                    fill="currentColor"></path>
+                                <path fillRule="evenodd" clipRule="evenodd"
+                                      d="M5 1C2.79086 1 1 2.79086 1 5V19C1 21.2091 2.79086 23 5 23H19C21.2091 23 23 21.2091 23 19V5C23 2.79086 21.2091 1 19 1H5ZM19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z"
+                                      fill="currentColor"></path>
+                            </svg>
+                        </a>
+                    </li>
+                    <li className="social-item">
+                        <a className="social-link" href="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                 fill="none">
+                                <path
+                                    d="M22.23 0H1.77C.79 0 0 .774 0 1.73V22.27C0 23.23.79 24 1.77 24H22.23C23.21 24 24 23.23 24 22.27V1.73C24 .774 23.21 0 22.23 0ZM7.12 20.45H3.56V9.02H7.12V20.45ZM5.34 7.56C4.19 7.56 3.26 6.63 3.26 5.48C3.26 4.34 4.19 3.41 5.34 3.41C6.48 3.41 7.42 4.34 7.42 5.48C7.42 6.63 6.48 7.56 5.34 7.56ZM20.45 20.45H16.88V14.8C16.88 13.61 16.87 12.07 15.18 12.07C13.47 12.07 13.24 13.42 13.24 14.7V20.45H9.68V9.02H13.1V10.4H13.15C13.66 9.55 14.7 8.65 16.21 8.65C19.4 8.65 19.4 11.06 19.4 14.13V20.45H20.45Z"
+                                    fill="currentColor"/>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
             </div>
-    )
+            <div className="footer-links">
+                <a href="/" className="text-gray-600 hover:text-gray-900">About Us</a>
+                <a href="/about" className="text-gray-600 hover:text-gray-900">Privacy Policy</a>
+                <a href="/contact" className="text-gray-600 hover:text-gray-900">Contact Us</a>
+            </div>
+
+            <div className="footer-links">
+                <a href="/services" className="text-gray-600 hover:text-gray-900">Terms & Conditions</a>
+                <a href="/services" className="text-gray-600 hover:text-gray-900">Refunds & Cancellations</a>
+                <a href="/services" className="text-gray-600 hover:text-gray-900">Terms & Conditions</a>
+            </div>
+
+            <div className="footer-copyright">
+                <p>Copyright © 2020 <a href="/">www.dishaai.com</a> - All rights reserved</p>
+            </div>
+        </footer>
+    );
 }
+
+export default Footer;
