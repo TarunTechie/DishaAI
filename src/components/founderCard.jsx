c69566ea746a4f2e566dfd831aa977e84963d5ba
tarun-techie <taruntechie25@gmail.com>
2024-06-20 01:37:58 +0530

    founders section done

--- src/components/founderCard.jsx
+++ src/components/founderCard.jsx
@@ -1,15 +1,22 @@
-export default function FounderCard()
-{
-    return(
-        <div className="grid  m-10 items-center shadow-md shadow-black/40 bg-[url('/images/card.jpg')] bg-cover rounded-ss-xl rounded-ee-xl xl:w-1/6">
-                <div className="grid justify-center bg-gradient-to-r from-blue-500 rounded-ss-xl">
-                    <img src="/test.png" className="w-44 rounded-full h-44 mt-8 shadow-lg shadow-black border-4 border-white" />
-                </div>
-            <div className="text-white bg-black/10 backdrop-blur p-4 text-center border-2 border-blue-500 rounded-ee-xl shadow-inner shadow-black">
-                <h1 className="text-center font-bold text-2xl underline underline-offset-4">Prahalad Karnam</h1>
-                <h1>Co-Founder & Director</h1>
-                <h1 className="pt-4 text-xl">21+ years of <br/>Global Experience</h1>
-            </div>
+export default function FounderCard() {
+    return (
+      <div className="w-full md:w-1/3 lg:w-1/5 h-96  m-4 relative founder perspective  rounded-xl">
+        <div className="founderf absolute inset-0 grid  rounded-lg bg-[url('/images/card.jpg')] bg-cover">
+          <div className="grid justify-center bg-gradient-to-r from-blue-500 rounded-lg">
+            <img src="/test.png" className="w-44 h-44 rounded-full mt-8 shadow-lg shadow-black border-4 border-white" />
+          </div>
+          <div className="text-white bg-white/40 backdrop-blur p-4 text-center rounded-lg shadow-inner shadow-black">
+            <h1 className="text-center font-bold text-lg md:text-xl lg:text-2xl underline underline-offset-4">Name</h1>
+            <h2 className="text-sm md:text-base lg:text-lg">Co-Founder & Director</h2>
+            <p className="pt-2 md:pt-4 text-sm md:text-base lg:text-xl">21+ years of <br />Global Experience</p>
+          </div>
         </div>
-    )
-}
+        <div className="founderb absolute inset-0 rounded-xl bg-[url('/images/card.jpg')] bg-cover">
+          <p className="bg-white/25 backdrop-blur p-8 h-96 md:p-6 lg:p-8 text-xs md:text-sm lg:text-base rounded-xl">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque doloribus, hic praesentium velit optio tempora porro expedita fugit laborum sed blanditiis autem fugiat, at iusto! Aspernatur quos debitis nesciunt maxime.
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
--- src/sections/founder.jsx
+++ src/sections/founder.jsx
@@ -1,13 +1,15 @@
-import FounderCard from "../components/founderCard"
-export default function Founder()
-{
-    return(
-        <div>
-            <h1 className="text-center text-5xl font-heading underline underline-offset-8">OUR TEAM</h1>
-        <div className="grid justify-center bg-white md:flex">
-            <FounderCard/>
-            <FounderCard/>
-        </div>
-        </div>
-    )
+import FounderCard from "../components/founderCard";
+
+export default function Founder() {
+  return (
+    <div className="min-h-screen bg-white/50 backdrop-blur py-8">
+      <h1 className="text-center text-3xl md:text-5xl font-heading underline underline-offset-8 mb-8">
+        OUR TEAM
+      </h1>
+      <div className="flex flex-wrap justify-center gap-8">
+        <FounderCard />
+        <FounderCard />
+      </div>
+    </div>
+  );
 }
